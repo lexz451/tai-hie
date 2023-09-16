5504f086d052a8fd2c6effa17460ce40a686a314
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2023-09-15 19:10:45 -0700

    feat: Add Inter font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ['InterVariable', 'Inter', ...defaultTheme.fontFamily.sans],
+			},
 			colors: {
 				"blue-dark": '#051231',
 				"blue": '#3d30bf',
 				"blue-light": '#5e48ff',
 				"light": "#ffffff",
+				"grey": "#6b7280",
 				"green-accent": "#00e8c3"
 			}
 		},
