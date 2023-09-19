2f8472e7f7f9d25c995ed770f665db7a815f3de2
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2023-09-19 01:22:37 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-  adapter: netlify({
-    edgeMiddleware: true
-  })
+  output: "static",
+  // adapter: netlify({
+  //   edgeMiddleware: false
+  // })
 });
