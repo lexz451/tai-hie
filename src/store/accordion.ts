f7b2cefe2e74fcf4ef1f9eba8e0c1116f6eca8a8
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2023-09-21 20:56:25 -0700

    feat: Updates #7

--- src/store/accordion.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from 'nanostores';
-
-export let activeItem = atom<string | null>(null);
-
-export const toggleItem = (key: string) => {
-    activeItem.set(activeItem.get() === key ? null : key);
-}
