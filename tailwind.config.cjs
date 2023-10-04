188835a9ea52ceaa949379b375714f0f105d1c50
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2023-10-04 14:52:41 -0700

    feat: Update
    Modal styles
    Favicon

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,11 +10,12 @@ module.exports = {
 			},
 			colors: {
 				"blue-dark": '#051231',
+				'blue-dark-translucent': 'rgba(5, 18, 49, 0.7)',
 				"blue": '#3d30bf',
 				"blue-light": '#5e48ff',
 				"light": "#ffffff",
 				"grey": "#6b7280",
-				"green-accent": "#00e8c3"
+				"green-accent": "#00e8c3",
 			}
 		},
 	},
